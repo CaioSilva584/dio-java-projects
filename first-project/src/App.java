80d88a4748a619e04ae7f2d7744df79f6c878c90
Caio <caiog.silva2001@gmail.com>
2024-05-14 12:47:30 -0300

    feat: First Java project added

---
+++ first-project/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Isto executa-se em Java, apesar de não sabermos a versão em que está.");
+    }
+}
