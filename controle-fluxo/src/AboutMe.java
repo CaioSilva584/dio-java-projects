459bb9fb791f8b1bba63eb4257ff7c1b1d7f0577
Caio <caiog.silva2001@gmail.com>
2024-05-21 14:19:37 -0300

    feat: Exceptions content added in project controle-fluxo

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Locale;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        try {
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Digite seu nome: ");
+            String nome = scan.next();
+
+            System.out.println("Digite seu nome: ");
+            String sobrenome = scan.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scan.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            double altura = scan.nextDouble();
+
+            System.out.println("Olá, me chamo " + nome.toUpperCase() +
+                    " " + sobrenome.toUpperCase() + "\nTenho " + idade +
+                    " anos\nMinha altura é " + altura + "m");
+
+            scan.close();
+        } catch (InputMismatchException e) {
+            System.out.println("*** Os campos idade e altura devem ser numéricos ***");
+        }
+    }
+}
---
+++ controle-fluxo/src/FormatadorCep.java
@@ -0,0 +1,21 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        // Tratamento decorrente do uso da classe "Exception"
+        try {
+            String cep = formatarCep("23765064");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+/* O uso de "throws Exception" torna obrigatório o tratamento de erro
+ * quando o método é utilizado.
+ */
+    public static String formatarCep (String cep) throws Exception{
+        if (cep.length() != 8){
+            throw new CepInvalidoException();
+        }
+
+        return "23.765-064";
+    }
+}
